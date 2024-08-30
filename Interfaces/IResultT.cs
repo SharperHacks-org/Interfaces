d304125620d4d7d959f46e21b4e027811f89c725
Joseph W Donahue <joseph@sharperhacks.com>
2024-08-29 17:39:15 -0700

    Initial commit.

---
+++ Interfaces/IResultT.cs
@@ -0,0 +1,39 @@
+﻿// Copyright and trademark notices at the end of this file.
+
+namespace SharperHacks.CoreLibs.Interfaces;
+
+/// <summary>
+/// A generic result type interface.
+/// </summary>
+/// <remarks>
+/// Can be used as a return type, but is probably more useful for cases where
+/// a class method returns one thing or another, and also retains it's final result.
+/// Not intended for use as state, except in the case of final state.
+/// </remarks>
+/// <typeparam name="T"></typeparam>
+public interface IResult<T>
+{
+    /// <summary>
+    /// Get the result.
+    /// </summary>
+    public T Result { get; }
+}
+
+// Copyright Joseph W Donahue and Sharper Hacks LLC (US-WA)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SharperHacks is a trademark of Sharper Hacks LLC (US-Wa), and may not be
+// applied to distributions of derivative works, without the express written
+// permission of a registered officer of Sharper Hacks LLC (US-WA).
+
---
+++ README.md
@@ -0,0 +1,35 @@
+![SharperHacks logo](SHLLC-Logo.jpg)
+# Interfaces
+## SharperHacks.CoreLibs.Interfaces
+
+A collection of useful interfaces, with zero implementation dependancies.
+
+Licensed under the Apache License, Version 2.0. See [LICENSE](LICENSE).
+
+Contact: joseph@sharperhacks.org
+
+Nuget: https://www.nuget.org/packages/SharperHacks.CoreLibs.Interfaces
+
+### Targets
+- net8.0
+- net9.0
+
+### Interfaces
+
+#### IInvoke
+Defines the Invoke() interface.
+
+#### IResultAccumulator
+A result accumulator interface.
+
+#### IResult&lt;T&gt;
+A generic result type interface.
+
+#### IRunable
+A Run() interface.
+
+#### ISettableState&lt;T&gt;
+A generic settable state interface.
+
+#### IState&lt;T&gt;
+A generic state interface.
