4ff503382800f2eaf5d505f6af1b4571cfd7e56b
Joseph W Donahue <joseph@sharperhacks.com>
2024-09-01 15:02:28 -0700

    NB: Add Invoke interface with a generic return type.

---
+++ Interfaces/IInvokeTResult.cs
@@ -0,0 +1,16 @@
+﻿// Copyright and trademark notices at the end of this file.
+
+namespace SharperHacks.CoreLibs.Interfaces;
+
+/// <summary>
+/// Defines the Invoke() interface with a generic return type.
+/// </summary>
+/// <typeparam name="TResult"></typeparam>
+public interface IInvoke<TResult>
+{
+    /// <summary>
+    /// Invoke any encapsulated function or process with a generic return type.
+    /// </summary>
+    /// <returns>TResult</returns>
+    public TResult Invoke();
+}
